e307f65b4f8d13f7a8289fb019487443f453f79e
Chris Queen <chrisqueen10@gmail.com>
2022-11-10 15:23:10 -0500

    update ui:

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
 import 'mapbox-gl/dist/mapbox-gl.css'
 import styles from './map.module.css'
@@ -11,12 +11,28 @@ export default function Map() {
   const mapContainer = useRef(null)
   const map = useRef(null)
 
+  const [lng, setLng] = useState()
+  const [lat, setLat] = useState()
+  const [zoom, setZoom] = useState()
+
   // Initial map setup
   useEffect(() => {
     if (map.current) return // initialize map only once
     if (!mapContainer?.current) return
 
     map.current = getMapConfig(mapContainer.current)
+
+    // listen for map changes and update UI
+    if (map.current) {
+      const updateUI = () => {
+        setLng(map.current.getCenter().lng.toFixed(4))
+        setLat(map.current.getCenter().lat.toFixed(4))
+        setZoom(map.current.getZoom().toFixed(2))
+      }
+
+      map.current.on('load', updateUI)
+      map.current.on('move', updateUI)
+    }
   }, [])
 
   return (
@@ -24,7 +40,7 @@ export default function Map() {
       <noscript>You need to enable JavaScript to run this app.</noscript>
       <div className={styles.sidebar}>
         {/* TODO need to grab values from state */}
-        Longitude: {map?.current?.getCenter().lng} | Latitude: {map?.current?.getCenter().lat} | Zoom: {map?.current?.getZoom()}
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div ref={mapContainer} className={styles.mapContainer} />
     </div>
