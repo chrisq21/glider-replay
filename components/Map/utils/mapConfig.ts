1b072afd85ac48da1f627fd05528fb9707b632d0
Chris Queen <chrisqueen10@gmail.com>
2022-11-11 17:34:37 -0500

    Update sky

--- components/Map/utils/mapConfig.ts
+++ components/Map/utils/mapConfig.ts
@@ -44,12 +44,13 @@ function configureMapStyles(map) {
   })
   map.setTerrain({source: 'mapbox-dem', exaggeration: 1})
 
-  // add sky styling with `setFog` that will show when the map is highly pitched
-  map.setFog({
-    'horizon-blend': 0.3,
-    color: '#f8f0e3',
-    'high-color': '#add8e6',
-    'space-color': '#d8f2ff',
-    'star-intensity': 0.0,
+  map.addLayer({
+    id: 'sky',
+    type: 'sky',
+    paint: {
+      'sky-type': 'atmosphere',
+      'sky-atmosphere-sun': [0.0, 0.0],
+      'sky-atmosphere-sun-intensity': 15,
+    },
   })
 }
